8340b11df6a672c305b65add36d59126a6f5a817
woxydster <woxydster@gmail.com>
2023-02-26 19:37:47 +0300

    add Example010 MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 21, 24, 31, 48, 25, 16, 57, 48 };
+
+int n = array.Length;
+int find = 48;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
